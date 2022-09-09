708dc8efa7514b3927cf7746820f4cc2c67f58cb
zemscky <aleksei.zemscky@yandex.ru>
2022-09-10 00:17:58 +0300

    Домашня работа 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+                //1
+                var dog = 8;
+                System.out.println(dog);
+                var cat = 3.6;
+                System.out.println(cat);
+                var paper = 763789;
+                System.out.println(paper);
+                //2
+                dog = dog + 4;
+                System.out.println(dog);
+                cat = cat + 4;
+                System.out.println(cat);
+                paper = paper + 4;
+                System.out.println(paper);
+                //3
+                var dog1 = 8.0;
+                dog1 = dog1 - 3.5;
+                System.out.println(dog1);
+                cat = cat - 1.6;
+                System.out.println(cat);
+                paper = paper - 7639;
+                System.out.println(paper);
+    }
+}
